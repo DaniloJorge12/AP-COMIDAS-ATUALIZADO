d70939b442b85c716074da404dc720ea7908012c
Danilo <danilojmini2@gmail.com>
2025-10-20 16:19:20 -0300

    Subindo arquivos para novo repositório

---
+++ src/routes/comidaRoute.js
@@ -0,0 +1,10 @@
+// Controla as rotas, ou seja, o caminho da URL
+import { Router } from "express";
+import * as comidaController from './../controllers/comidaControllers.js'
+
+const router = Router();
+
+router.get("/", comidaController.listarTodos);
+router.get("/:id", comidaController.listarUm);
+
+export default router;
